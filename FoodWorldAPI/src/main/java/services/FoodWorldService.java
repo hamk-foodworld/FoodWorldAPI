0363923e930d7bf31e414e8c22534d4c99c381dc
Drollbaer <marcosax95@web.de>
2020-09-29 11:47:30 +0300

    First commit -Test

---
+++ FoodWorldAPI/src/main/java/services/FoodWorldService.java
@@ -0,0 +1,19 @@
+package services;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
+
+@Path ("/foodservice")
+public class FoodWorldService {
+
+	@GET
+	@Path("/test")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String serviceName() {
+		return "This is a FoodWorld service";
+	}
+}
